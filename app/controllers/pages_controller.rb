02a04f934233531948e9c8563033bef5e88c95f9
Olivia Zorn <livzorn@gmail.com>
2020-11-25 14:00:45 +0000

    added logic to rotate messages, but they rotate every time you refresh, not every day

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,13 @@
 class PagesController < ApplicationController
-
   # skip_before_action :authenticate_user!, only: :home
 
-
   def home
-    # messages = []
-    # current_user.message_sets.each do |message_set|
-    #   @messages += message_set.messages
-    # end
-    # messages.flatten!
-    # @message = messages.sample
+    messages = []
+    current_user.message_sets.each do |message_set|
+      messages += message_set.messages
+    end
+    messages.flatten!
+    @message = messages.sample
     treats = ['https://www.youtube.com/watch?v=ynLpZGegiJE', 'https://www.sdzsafaripark.org/giraffe-cam', 'https://www.youtube.com/watch?v=91wX0NRjJqg']
     @treat = treats.sample
   end
