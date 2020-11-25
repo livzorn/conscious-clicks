b4e57c7634e5812d866cfba8ccd95f88ddb3f137
Giorgiana Arco Zinneberg <garcozinneberg@gmail.com>
2020-11-25 12:16:13 +0000

    homepage, needs work which I will be doing tonight- pushing now so we can do backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
+  root to: 'pages#landing'
   get '/login', to: 'users#login'
 end
