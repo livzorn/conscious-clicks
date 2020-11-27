53efaf8776302331d3968ccc0ee1e52bf2357234
Nikola Calic <chala021@gmail.com>
2020-11-27 13:50:32 +0100

    reg added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
-   before_action :authenticate_user!
+   before_action :authenticate_user!, except: :landing
+   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+   private
+   def after_sign_out_path_for(resource_or_scope)
+    landing_path
+  end
+
+  def after_sign_up_path_for(resource)
+    "/quiz" # <- Path you want to redirect the user to.
+  end
 end
