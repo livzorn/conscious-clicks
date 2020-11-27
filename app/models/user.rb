78dcee2fc1592b2b998d6c268d118af3b6e7d037
Olivia Zorn <livzorn@gmail.com>
2020-11-27 16:02:04 +0000

    fixed stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :user_moods
-  has_many :bookmarks
-  has_many :user_message_sets
+  has_many :user_moods, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
+  has_many :user_message_sets, dependent: :destroy
   has_many :moods, through: :user_moods
   has_many :message_sets, through: :user_message_sets
+  has_many :goals, dependent: :destroy
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 end
