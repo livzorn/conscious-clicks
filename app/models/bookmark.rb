fc4777f0389cb986d68deb27a60e95e0c08a0102
Giorgiana Arco Zinneberg <garcozinneberg@gmail.com>
2020-11-27 13:02:17 +0000

    icons in :D

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,8 @@
 class Bookmark < ApplicationRecord
   belongs_to :user
   validates :link, :name, presence: true
+
+  def favicon_url
+    "https://i.olsh.me/icon?url=#{link}&size=80..120..200"
+  end
 end
