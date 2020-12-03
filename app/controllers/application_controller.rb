22e43641d890230b37e58757e687f7e66041fb9f
Olivia Zorn <livzorn@gmail.com>
2020-12-03 17:24:43 +0000

    added meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,8 @@ class ApplicationController < ActionController::Base
   def after_sign_up_path_for(resource)
     "/quiz" # <- Path you want to redirect the user to.
   end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
